90c655793d68b2477e723baf2a22596be4eb1e53
Abhishek Prakash Bawane <120009589+AbhishekBawane@users.noreply.github.com>
2026-01-26 01:28:23 +0530

    Temporary minimal API test

--- api/blogs.ts
+++ api/blogs.ts
@@ -1,6 +1,5 @@
-import { blogs } from "../db.json";
-import { randomUUID } from "crypto";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { blogs } from "../data/blogs";
 
 export default function handler(
   req: VercelRequest,
@@ -10,27 +9,5 @@ export default function handler(
     return res.status(200).json(blogs);
   }
 
-  if (req.method === "POST") {
-    const { title, description, content, category, coverImage } = req.body;
-
-    if (!title || !content) {
-      return res.status(400).json({ message: "Missing fields" });
-    }
-
-    const newBlog = {
-      id: randomUUID(),
-      title,
-      description,
-      content,
-      category,
-      coverImage,
-      date: new Date().toISOString(),
-    };
-
-    blogs.unshift(newBlog);
-
-    return res.status(201).json(newBlog);
-  }
-
-  return res.status(405).json({ message: "Method not allowed" });
+  res.status(405).json({ message: "Method not allowed" });
 }
